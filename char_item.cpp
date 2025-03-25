078e1a7db19d4ae9317364983e0021b348ac6a1e
Waesis <100868091+Waesis@users.noreply.github.com>
2025-03-25 15:32:52 +0200

    Update char_item.cpp

--- char_item.cpp
+++ char_item.cpp
@@ -1,53 +1,86 @@
-Search :
+#include service.h
+ //Search :
 AggregateMonster();
-
-Replace with :
-
+  
+///Replace with :
 #ifdef ENABLE_BRAVE_CAPE_WITH_MONEY
-								if(GetLevel() <= 30)
+#ifdef LEVEL_LIMIT_FOR_TAX_PER_USE
+								// Level req
+								static int incepator = 30;
+								static int mediu = 60;
+								static int avansat = 90;
+								static int maestru = 120;
+								//  Money Request
+								static int LowLevel = 5 * 1000; //  5000
+								static int MediumLevel = 10* 1000; // 10000
+								static int MaxLevel = 25 * 1000; // 25000
+
+
+#endif 
+								if(GetLevel() <= incepator )
 								{
 									// Under Level 30 don`t take taxe
+								#ifdef EFFECT_BRAVE_CAPE
 									this->EffectPacket(SE_MANTELLO);
+								#endif
+								#ifdef ENABLE_RANGE_MONSTER_BRAVE_CAPE
+									AggregateRangeMonster();
+								#endif
 									AggregateMonster();
 								}
-								else if(GetLevel() <= 60)
+								else if(GetLevel() <= mediu)
 								{
-									static int LowLevel = 5000;
+
 									if (GetGold() < LowLevel) 
 									{
 										ChatPacket(CHAT_TYPE_INFO, "<Cape System> You need 5000 yang.");
 									}
 									else
 									{
+										#ifdef EFFECT_BRAVE_CAPE
 										this->EffectPacket(SE_MANTELLO);
+									#endif
 										AggregateMonster();
+									#ifdef ENABLE_RANGE_MONSTER_BRAVE_CAPE
+										AggregateRangeMonster();
+									#endif
 										PointChange(POINT_GOLD, -LowLevel);//5k yang
 									}
 								}
-								else if(GetLevel() <= 90)
+								else if(GetLevel() <= avansat)
 								{
-									static int MediumLevel = 10000;
+
 									if (GetGold() < MediumLevel) 
 									{
 										ChatPacket(CHAT_TYPE_INFO, "<Cape System> You need 10000 yang.");
 									}
 									else
 									{
+										#ifdef EFFECT_BRAVE_CAPE
 										this->EffectPacket(SE_MANTELLO);
+									#endif
 										AggregateMonster();
+									#ifdef ENABLE_RANGE_MONSTER_BRAVE_CAPE
+										AggregateRangeMonster();
+									#endif
 										PointChange(POINT_GOLD, -MediumLevel);//10k yang
 									}
 								}
-								else if(GetLevel() <=120)
+								else if(GetLevel() <= maestru)
 								{
-									static int MaxLevel = 25000;
+
 									if (GetGold() < MaxLevel) 
 									{
 										ChatPacket(CHAT_TYPE_INFO, "<Cape System> You need 25000 yang.");
 									}
 									else
 									{
+										#ifdef EFFECT_BRAVE_CAPE
 										this->EffectPacket(SE_MANTELLO);
+									#endif
+									#ifdef ENABLE_RANGE_MONSTER_BRAVE_CAPE
+										AggregateRangeMonster();
+									#endif
 										AggregateMonster();
 										PointChange(POINT_GOLD, -MaxLevel);//25k yang
 									}
