3448b983554358969f45f6dc9c3bce4cea906798
Waesis <100868091+Waesis@users.noreply.github.com>
2025-03-26 12:53:20 +0200

    Update service.h
    Level request & money for  use on define.

--- service.h
+++ service.h
@@ -5,4 +5,21 @@
     #define BUFF_RAGE_BRAVE_CAPE
     #define ENABLE_RANGE_MONSTER_BRAVE_CAPE
     #define LEVEL_LIMIT_FOR_TAX_PER_USE
+
+
+    #ifdef LEVEL_LIMIT_FOR_TAX_PER_USE
+    // Level req
+    #define incepator 30;
+    #define mediu  60;
+    #define avansat 90;
+    #define master  120;
+    //  Money Request
+    #define LowLevel  5000; //  5000
+    #define MediumLevel  10o00; // 10000
+    #define MaxLevel 25000; // 25000
+#endif 
+
+
 #endif
+
+
